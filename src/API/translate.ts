9cf6fb5f6e2abac3b108b604a773f7cd9bbe98dd
biubiujch <coolechi@foxmail.com>
2022-09-11 10:47:30 +0800

    feat: 新增文案api

--- src/API/translate.ts
+++ src/API/translate.ts
@@ -3,84 +3,91 @@ import { filterQuery, generateID } from "../utils/utils";
 import { Source } from "../model/source";
 import { Target } from "../model/target";
 
+
 const router = Router();
 
 
 router.get("/getAll", async function (req, res) {
   try {
-    const { projectID } = filterQuery<SourceKeys & Targetkeys>(req.query)
+    const { projectID } = filterQuery<SourceKeys & Targetkeys>(req.query);
     const data = await Source.findAll({
       where: {
-        projectID
-      }
-    })
+        projectID,
+      },
+    });
     const dstData = await Target.findAll({
       where: {
-        projectID
-      }
-    })
-    const result: { dst: Target[]; id: string; projectID: String; key: string; text: string; from: string; }[] = []
+        projectID,
+      },
+    });
+    const result: { dst: Target[]; id: string; projectID: String; key: string; text: string; from: string }[] = [];
     data.forEach((item) => {
-      const value = item.get()
-      const dst = dstData.filter((e) => e.get().srcID === value.id)
+      const value = item.get();
+      const dst = dstData.filter((e) => e.get().srcID === value.id);
       result.push({
         ...value,
-        dst
-      })
-    })
-    res.send(result)
+        dst,
+      });
+    });
+    res.send(result);
   } catch (e) {
-    console.log(e)
-    res.status(500).json("system busy")
+    console.log(e);
+    res.status(500).json("system busy");
   }
-})
+});
 
 router.post("/create", async function (req, res) {
   try {
-    const query = filterQuery<SourceKeys & { dst: { to: string, text: string }[] }>(req.query)
-    const data = await Source.findOne({ where: { key: query.key } })
+    const { projectID, from, text, key, dst } = filterQuery<SourceKeys & { dst: { to: string; text: string }[] }>(
+      req.body
+    );
+    console.log(req.body);
+    const data = await Source.findOne({ where: { projectID, key } });
     if (data !== null) {
-      res.status(400).json("item exist")
-      return
+      res.status(400).json("item exist");
+      return;
     }
+
     const result = await Source.create({
-      ...query,
-      id: generateID()
-    })
-    for await (const item of query.dst) {
+      id: generateID(),
+      projectID,
+      from,
+      text,
+      key,
+    });
+    for await (const item of dst) {
       await Target.create({
         id: generateID(),
-        projectID: query.projectID,
+        projectID: projectID,
         srcID: result.get().id,
         text: item.text,
-        to: item.to
-      })
+        to: item.to,
+      });
     }
-    res.send(result)
+    res.send(result);
   } catch (e) {
-    res.status(500).json("system busy")
+    console.log(e);
+    res.status(500).json("system busy");
   }
-})
+});
 
 router.post("/delete", async function (req, res) {
   try {
-    const { id } = filterQuery<SourceKeys>(req.query)
+    const { id } = filterQuery<SourceKeys>(req.query);
     const data = await Source.destroy({
       where: {
-        id
-      }
-    })
+        id,
+      },
+    });
     await Target.destroy({
       where: {
-        srcID: id
-      }
-    })
-    res.send(data)
+        srcID: id,
+      },
+    });
+    res.send(data);
   } catch (e) {
-    res.status(500).json("system busy")
+    res.status(500).json("system busy");
   }
-})
-
-
+});
 
 export default router;
--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,16 @@ import project from "./API/project";
 import administrator from "./API/administrator";
 import translate from "./API/translate";
 import "./utils/parseENV";
+import parser from "body-parser";
 
 const App = express();
 App.all("*", (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 
+App.use(parser.json());
 App.use("/api/project", project);
 App.use("/api/administrator", administrator);
 App.use("/api/translate", translate);
